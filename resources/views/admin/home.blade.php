1993355d2be3833426179d490836029715b81ff1
hapo-xuantt <xuantt@haposoft.com>
2020-04-26 11:16:34 +0700

    login & crud user

---
+++ resources/views/admin/home.blade.php
@@ -0,0 +1,173 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+<div class="wrapper">
+
+    <!-- Navbar -->
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+        <!-- Left navbar links -->
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+            </li>
+            <li class="nav-item d-none d-sm-inline-block">
+                <a href="index3.html" class="nav-link">Home</a>
+            </li>
+            <li class="nav-item d-none d-sm-inline-block">
+                <a href="#" class="nav-link">Contact</a>
+            </li>
+        </ul>
+
+        <!-- SEARCH FORM -->
+        <form class="form-inline ml-3">
+            <div class="input-group input-group-sm">
+                <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+                <div class="input-group-append">
+                    <button class="btn btn-navbar" type="submit">
+                        <i class="fas fa-search"></i>
+                    </button>
+                </div>
+            </div>
+        </form>
+
+        <!-- Right navbar links -->
+        <div class="navbar-nav ml-auto">
+            @auth
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }} <span class="caret"></span>
+                    </a>
+
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                            {{ __('Logout') }}
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </div>
+                </li>
+            @endauth
+        </div>
+    </nav>
+    <!-- /.navbar -->
+
+    <!-- Main Sidebar Container -->
+    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+        <!-- Brand Logo -->
+        <a href="index3.html" class="brand-link">
+            <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+                 style="opacity: .8">
+            <span class="brand-text font-weight-light">AdminLTE 3</span>
+        </a>
+
+        <!-- Sidebar -->
+        <div class="sidebar">
+            <!-- Sidebar user panel (optional) -->
+            <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                <div class="image">
+                    <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                </div>
+                <div class="info">
+                    <a href="#" class="d-block">Alexander Pierce</a>
+                </div>
+            </div>
+
+            <!-- Sidebar Menu -->
+            <nav class="mt-2">
+                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                    <!-- Add icons to the links using the .nav-icon class
+                         with font-awesome or any other icon font library -->
+                    <li class="nav-item has-treeview menu-open">
+                        <a href="#" class="nav-link active">
+                            <i class="nav-icon fas fa-tachometer-alt"></i>
+                            <p>
+                                Dashboard
+                                <i class="right fas fa-angle-left"></i>
+                            </p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="pages/widgets.html" class="nav-link">
+                            <i class="nav-icon fas fa-th"></i>
+                            <p>
+                                Chương trình khuyến mãi
+                            </p>
+                        </a>
+                    </li>
+                    <li class="nav-item has-treeview">
+                        <a href="#" class="nav-link">
+                            <i class="nav-icon fas fa-copy"></i>
+                            <p>
+                                Thể loại
+                                <i class="fas fa-angle-left right"></i>
+                            </p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ route('users.index') }}" class="nav-link">
+                            <i class="nav-icon fas fa-th"></i>
+                            <p>
+                                Users
+                            </p>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+            <!-- /.sidebar-menu -->
+        </div>
+        <!-- /.sidebar -->
+    </aside>
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Main content -->
+        <section class="content mt-2">
+            <div class="container-fluid">
+               @yield('content')
+            </div>
+        </section>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+    <footer class="main-footer">
+        <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
+        All rights reserved.
+        <div class="float-right d-none d-sm-inline-block">
+            <b>Version</b> 3.0.4
+        </div>
+    </footer>
+
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+        <!-- Control sidebar content goes here -->
+    </aside>
+    <!-- /.control-sidebar -->
+</div>
+</body>
+</html>
+
+
---
+++ resources/views/admin/users/edit.blade.php
@@ -0,0 +1,76 @@
+@extends('admin.home')
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">
+                <i class="fas fa-chart-pie mr-1"></i>
+                Cập nh người dùng
+            </h3>
+        </div>
+        <div class="card-body">
+            <form action="{{ route('users.update', $user->id) }}" method="POST">
+                @method('PATCH')
+                @csrf
+                <div class="form-group">
+                    <lable>Họ và tên</lable>
+                    <input type="text" name="name" placeholder="Họ và tên" class="form-control" value="{{ old('name', $user->name) }}">
+                    @error('name')
+                    <strong class="text-danger"> {{ $message }}</strong>
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <lable>Ngày sinh</lable>
+                    <input type="date" name="birthday" placeholder="Ngày sinh" class="form-control" value="{{ old('birthday', $user->birthday) }}">
+                    @error('birthday')
+                    <strong class="text-danger"> {{ $message }}</strong>
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <lable>Email</lable>
+                    <input type="email" name="email" placeholder="Email" class="form-control" value="{{ old('email', $user->email) }}">
+                    @error('email')
+                    <strong class="text-danger"> {{ $message }}</strong>
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <lable>Giới tính</lable>
+                    <select class="form-control" name="gender">
+                        @foreach(\App\User::GENDER as $key => $value)
+                            <option value="{{ $key }}" {{ old('gender', $user->gender) == $key ? 'selected' : '' }}>{{ $value }}</option>
+                        @endforeach
+                    </select>
+                    @error('gender')
+                    <strong class="text-danger"> {{ $message }}</strong>
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <lable>Số điện thoại</lable>
+                    <input type="text" name="phone" placeholder="phone" class="form-control" value="{{ old('phone', $user->phone) }}">
+                    @error('phone')
+                    <strong class="text-danger"> {{ $message }}</strong>
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <lable>Địa chỉ</lable>
+                    <input type="text" name="address" placeholder="address" class="form-control" value="{{ old('address', $user->address) }}">
+                    @error('address')
+                    <strong class="text-danger"> {{ $message }}</strong>
+                    @enderror
+                </div>
+                <div class="form-group">
+                    <lable>Quyền</lable>
+                    <select class="form-control" name="role">
+                        @foreach(\App\User::ROLE as $key => $value)
+                            <option value="{{ $key }}" {{ old('role', $user->role) == $key ? 'selected' : '' }}>{{ $value }}</option>
+                        @endforeach
+                    </select>
+                    @error('role')
+                    <strong class="alert alert-danger"> {{ $message }}</strong>
+                    @enderror
+                </div>
+                <button class="btn btn-success" type="submit">Cập nhật</button>
+            </form>
+        </div>
+    </div>
+@endsection
